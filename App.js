d8617e5529d2a98a1cff27e313790f7c5cc16cc0
yemimamds <sihmirahmds@gmail.com>
2024-06-22 09:44:16 +0700

    Initial commit with NoteApp features

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import Home from './src/screens/home'
+import AddNote from './src/screens/addNote'
+import EditNote from './src/screens/editNote'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//import { View, StyleSheet } from 'react-native'
+//import CustomButton from './src/components/customButton'
+//import CustomTextInput from './src/components/customTextInput'
+
+const CurrentPageWidget = ({
+  currentPage,
+  noteList,
+  setCurrentPage,
+  addNote,
+}) => {
+  switch (currentPage) {
+    case 'home':
+      return <Home noteList={noteList} setCurrentPage={setCurrentPage} />
+    case 'add':
+      // Berikan function "addNote" ke component "AddNote"
+      return <AddNote setCurrentPage={setCurrentPage} addNote={addNote} />
+    case 'edit':
+      return <EditNote />
+    default:
+      return <Home />
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+      const [currentPage, setCurrentPage] = useState('home')
+    
+      const [noteList, setNoteList] = useState([
+        {
+          id: 1,
+          title: 'Note pertama',
+          desc:
+            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        },
+      ])
+ 
+      const addNote = (title, desc) => {
+        const id = noteList.length > 0 ? noteList[noteList.length - 1].id + 1 : 1;
+        setNoteList([
+          ...noteList,
+          {
+            id,
+            title: title,
+            desc: desc,
+          },
+        ]);
+      };
+  
+
+  return (
+    <CurrentPageWidget
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      noteList={noteList}
+       // Berikan function addNote sebagai prop
+      addNote={addNote}
+    />
+  )
+}
+
+export default App
